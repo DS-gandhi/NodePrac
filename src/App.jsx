b6f25e455e4cc38e7c150ebd2a62e1f94b58b256
dhruvi <dhruvi.sg17@gmail.com>
2025-07-04 10:02:32 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello from Jenkins</h1>
+      <h1>Hello world, Jenkins here</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
